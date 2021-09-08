1b82ba65c5bdf7f0f5dced29c5aa93a6e12b9950
Martin Ueding <mu@martin-ueding.de>
2021-09-08 15:42:44 +0200

    Add transition function

--- src/game_simulation_sandbox/gameoflife/test_transition.py
+++ src/game_simulation_sandbox/gameoflife/test_transition.py
@@ -6,3 +6,17 @@ def test_padding() -> None:
     expected = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
     padded = add_padding(input)
     assert np.all(padded == expected)
+
+
+def test_remove_padding() -> None:
+    input = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    expected = np.array([[1]])
+    shrunk = remove_padding(input)
+    assert np.all(shrunk == expected)
+
+
+def test_next_state() -> None:
+    initial = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+    expected = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+    next = next_state(initial)
+    assert np.all(next == expected)
--- src/game_simulation_sandbox/gameoflife/transition.py
+++ src/game_simulation_sandbox/gameoflife/transition.py
@@ -1,7 +1,34 @@
 from typing import *
 
 import numpy as np
+import scipy.signal
+
+
+def next_state(state: np.ndarray) -> np.ndarray:
+    kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+    neighbors = scipy.signal.convolve2d(state, kernel, mode="same")
+    assert np.all(state.shape == neighbors.shape)
+    new_state = 1 * ((state == 1) & ((neighbors == 2) | (neighbors == 3))) | (
+        (state == 0) & (neighbors == 3)
+    )
+    return new_state
 
 
 def add_padding(state: np.ndarray) -> np.ndarray:
     return np.pad(state, 1, "constant")
+
+
+def remove_padding(state: np.ndarray) -> np.ndarray:
+    for min1 in range(state.shape[0]):
+        if np.any(state[min1, :] != 0):
+            break
+    for max1 in range(state.shape[0] - 1, -1, -1):
+        if np.any(state[max1, :] != 0):
+            break
+    for min2 in range(state.shape[1]):
+        if np.any(state[:, min2] != 0):
+            break
+    for max2 in range(state.shape[1] - 1, -1, -1):
+        if np.any(state[:, max2] != 0):
+            break
+    return state[min1 : max1 + 1, min2 : max2 + 1]
