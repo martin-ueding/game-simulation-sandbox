8895ff26d34080aff532859f9e81a1d99782853c
Martin Ueding <mu@martin-ueding.de>
2022-02-26 20:25:01 +0100

    Add some backtracking for railroad ink

---
+++ game_simulation_sandbox/railroadink/board.py
@@ -0,0 +1,75 @@
+from typing import List
+from typing import Optional
+
+from .tiles import available_tiles
+from .tiles import Direction
+from .tiles import FitType
+from .tiles import Tile
+from .tiles import TransportType
+
+board_size = 5
+board: List[List[Optional[Tile]]] = []
+for i in range(board_size):
+    board.append([None] * board_size)
+
+board[2][0] = Tile(
+    "exit rail",
+    "     \n     \n  #++\n     \n     ",
+    [[(Direction.RIGHT, TransportType.RAIL)]],
+)
+
+
+steps = 10
+
+
+def print_board():
+    result = []
+    for i in range(board_size * 5):
+        result.append([])
+
+    for i in range(board_size):
+        for j in range(board_size):
+            for k in range(5):
+                tile = board[i][j]
+                row = tile.image[k] if tile else "     "
+                result[i * 5 + k].append(row)
+
+    print("@")
+    for row in result:
+        print("".join(row))
+
+    global steps
+    if steps < 0:
+        raise RuntimeError()
+    steps -= 1
+
+
+def do_step():
+    print_board()
+    for tile in available_tiles:
+        for i in range(1, board_size - 1):
+            for j in range(1, board_size - 1):
+                if board[i][j] is not None:
+                    continue
+
+                fits = [
+                    tile.fits(board[i][j - 1], Direction.LEFT),
+                    tile.fits(board[i][j + 1], Direction.RIGHT),
+                    tile.fits(board[i - 1][j], Direction.UP),
+                    tile.fits(board[i + 1][j], Direction.DOWN),
+                ]
+
+                if all(fit != FitType.INCOMPATIBLE for fit in fits) and any(
+                    fit == FitType.MATCHES for fit in fits
+                ):
+                    board[i][j] = tile
+                    do_step()
+                    board[i][j] = None
+
+
+def main():
+    do_step()
+
+
+if __name__ == "__main__":
+    main()
--- game_simulation_sandbox/railroadink/tiles.py
+++ game_simulation_sandbox/railroadink/tiles.py
@@ -1,26 +1,92 @@
 import dataclasses
 import enum
 from typing import List
+from typing import Optional
 from typing import Tuple
 
 
 class Direction(enum.Enum):
-    RIGHT
-    UP
-    LEFT
-    DOWN
+    RIGHT = 1
+    UP = 2
+    LEFT = 3
+    DOWN = 4
+
+
+opposite_directions = {
+    Direction.RIGHT: Direction.LEFT,
+    Direction.UP: Direction.DOWN,
+    Direction.DOWN: Direction.UP,
+    Direction.LEFT: Direction.RIGHT,
+}
+
+
+class TransportType(enum.Enum):
+    RAIL = 1
+    ROAD = 2
+
+
+class FitType(enum.Enum):
+    MATCHES = 1
+    OPEN = 2
+    INCOMPATIBLE = 3
 
 
 class Tile:
-    image: str
-    connections: List[Tuple[Direction]]
+    def __init__(
+        self,
+        name: str,
+        image: str,
+        connections: List[List[Tuple[Direction, TransportType]]],
+    ):
+        self.name = name
+        self.image = image.split("\n")
+        self.connections = connections
+
+        assert len(self.image) == 5
+        assert all(len(row) == 5 for row in self.image)
+
+    def fits(
+        self,
+        other: Optional["Tile"],
+        direction_from_self: Direction,
+    ) -> FitType:
+        if other is None:
+            return FitType.OPEN
+
+        our_outgoing = None
+        for connection in self.connections:
+            for vertex in connection:
+                if vertex[0] == direction_from_self:
+                    our_outgoing = vertex[1]
+
+        their_outgoing = None
+        for connection in other.connections:
+            for vertex in connection:
+                if vertex[0] == opposite_directions[direction_from_self]:
+                    their_outgoing = vertex[1]
+
+        if our_outgoing is None and their_outgoing is None:
+            result = FitType.OPEN
+        elif our_outgoing == their_outgoing:
+            result = FitType.MATCHES
+        else:
+            result = FitType.INCOMPATIBLE
+        # print(f"Fit: {self}, {other}, {direction_from_self}: {result}")
+        return result
+
+    def __str__(self) -> str:
+        return self.name
 
 
-straight_rail = """
-
-"""
-
-
-tiles = {
-    "straight_rail": straight_rail,
-}
+available_tiles = [
+    Tile(
+        "straight rail",
+        "     \n     \n+++++\n     \n     ",
+        [[(Direction.LEFT, TransportType.RAIL), (Direction.RIGHT, TransportType.RAIL)]],
+    ),
+    Tile(
+        "straight road",
+        "     \n     \n.....\n     \n     ",
+        [[(Direction.LEFT, TransportType.ROAD), (Direction.RIGHT, TransportType.ROAD)]],
+    ),
+]
